8b2d25ed1b45024a7bdd7f0aad8ab6e16a6dc37c
jexxepinkman <157003294+jexxepinkman@users.noreply.github.com>
2024-02-15 18:00:36 +0900

    561
    9+5

--- Eulyoo/js/main.js
+++ Eulyoo/js/main.js
@@ -22,6 +22,34 @@ $(document).ready(function(){
         },
     
     });
-    swiper.autoplay.stop();  // 정지
-    swiper.autoplay.start();  // 재생
+
+    
+    //팝업 재생/정지
+    $('.visual .swiper .stop').on('click', function(){
+        swiper.autoplay.stop()
+        $('.visual .swiper .play').show() //숨기기
+        $('.visual .swiper .stop').hide() //보이기
+    })
+    $('.visual .swiper .play').on('click', function(){
+        swiper.autoplay.start()
+        $('.visual .swiper .play').hide()
+        $('.visual .swiper .stop').show()
+    })
+
+    //스크롤시 .fixed 추가, 맨 위에 있을시 .fixed 제거
+    let scrolling
+
+    function scroll_chk(){ //함수 선언
+        scrolling = $(window).scrollTop() //스크롤 된 값을 저장
+        console.log(scrolling)
+        if(scrolling > 0 ){
+            $('.header').addClass('fixed')
+        }else{
+            $('.header').removeClass('fixed')
+        }
+    }
+    scroll_chk() //함수 실행
+    $(window).scroll(function(){ //스크롤 할 때마다 실행
+        scroll_chk()
+    })
 })
