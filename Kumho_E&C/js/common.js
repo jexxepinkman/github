7f383dd68a4d5f6cf0bb614b5fd0a89a201ef487
jexxepinkman <157003294+jexxepinkman@users.noreply.github.com>
2024-02-28 18:00:01 +0900

    gergeg
    ergege

---
+++ Kumho_E&C/js/common.js
@@ -0,0 +1,6 @@
+/*  
+    file; common.js
+    editor; jaemin_Kim
+    date; 2024.02.28
+    description; 모든 페이지에서 사용 (header, footer)
+*/
---
+++ Kumho_E&C/js/main.js
@@ -0,0 +1,6 @@
+/*  
+    file; main.js
+    editor; jaemin_Kim
+    date; 2024.02.28
+    description; 메인페이지에만 사용 (header 제외)
+*/
